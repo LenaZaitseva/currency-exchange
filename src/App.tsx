142366521c2848a6d1c1fa2408db0fadc9a4415e
LenaZaitseva <elena.ivanova013@gmail.com>
2022-05-18 13:23:40 +0300

    chore: create project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout, AllCurrencyExchange, HomePage } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="all-exchange" element={<AllCurrencyExchange />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace={true} />} />
+    </Routes>
   );
 }
 
